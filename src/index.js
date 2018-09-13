fd81f0f471aca47f93701d1f5e71eb6ea7db61e6
grnkvch <43145804+grnkvch@users.noreply.github.com>
2018-09-13 10:03:25 +0300

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -3,5 +3,23 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  var count = 0;
+  var a, b, c;
+  var arr=preferences.slice();
+  for (var i = 0; i<arr.length; i++) {
+    if (arr[i]) {
+      a = arr[i];
+      b = arr[a-1];
+      c = arr[b-1];
+        if (a != b || b !=c){
+         if (i+1 === c) {
+          count++;
+          arr[i] = '';
+          arr[a-1] = '';
+          arr[b-1] = '';
+        }
+      }
+    }
+  }
+  return count;
 };
